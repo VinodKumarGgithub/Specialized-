cb3f120f17189beff4ad41ac96b636328a4bc1cb
Himanshu Sharan <sharanhemant7@gmail.com>
2023-05-16 21:29:07 +0530

    working on category page

---
+++ category/category.js
@@ -0,0 +1,40 @@
+let cycledata = [
+    {
+        "image": "https://www.specialized.com/s-works-epic-evo-ltd/p/218876?color=356880-218876",
+        "name": "S-Works Epic EVO LTD",
+        "price": "S$19,000.00",
+        "mrp": ""
+    },
+    {
+        "image": "https://www.specialized.com/s-works-epic-ltd/p/218875?color=356862-218875",
+        "name": "S-Works Epic LTD",
+        "price": "S$18,000.00",
+        "mrp": ""
+    },
+    {
+        "image": "https://www.specialized.com/epic-pro-ltd/p/218877?color=356881-218877",
+        "name": "Epic Pro LTD",
+        "price": "S$13,000.00",
+        "mrp": ""
+    },
+    {
+        "image": "https://www.specialized.com/epic-evo-pro-ltd/p/218878?color=356863-218878",
+        "name": "Epic EVO Pro LTD",
+        "price": "S$12,000.00",
+        "mrp": ""
+    },
+    {
+        "image": "https://www.specialized.com/s-works-epic-world-cup/p/206077?color=349286-206077",
+        "name": "S-Works Epic World Cup",
+        "price": "S$18,000.00",
+        "mrp": ""
+    },
+    {
+        "image": "https://www.specialized.com/tarmac-sl7-comp--shimano-105-di2/p/257114?color=396144-257114",
+        "name": "Tarmac SL7 Comp -Shimano 105 Di2",
+        "price": "S$8,300.00",
+        "mrp": ""
+    }
+]
+
+console.log(cycledata);
