06880294af3c3710ef6f95c687de4865da514024
VinodKumarGgithub <vpvinu.com@gmail.com>
2023-05-18 10:50:02 +0530

    api

--- index.js
+++ index.js
@@ -41,22 +41,45 @@ data.map(function(ele,i){
     let button_box = document.createElement("div")
     let wish_btn =document.createElement("button")
     wish_btn.innerHTML=`<i class="fa-regular fa-heart fa-lg"></i>`
+    wish_btn.style.color="black"
     wish_btn.addEventListener("click",()=>{
-        console.log(ele.id);
+        wish_btn.style.color= wish_btn.style.color=="black"? "red" : "black"
+        
     })
     let compare_btn = document.createElement("button")
     compare_btn.innerHTML=`<i class="fa-solid fa-code-compare"></i>`
+    compare_btn.style.color="black"
+    compare_btn.addEventListener("click",()=>{
+        compare_btn.style.color= compare_btn.style.color=="black"? "green":"black"
+    })
     button_box.append(wish_btn,compare_btn)
     box.append(img,title,price,button_box)
-    if(i>10&&i<=13){
+    if(i<3){
     document.querySelector(".h_bcycles").append(box)
-   }else if(i>13&&i<17) {
+   }else if(i==29 || i>10&&i<13) {
     document.querySelector("#bikesDis").append(box)
   }
 })
 }
 
 
+
+
+
+
+// slider
+let slider = document.getElementsByName("slider")
+console.log(document.getElementsByName("slider"));
+let slide_c=0
+
+setInterval(()=>{
+    slider[slide_c].checked=true
+if(slide_c==slider.length-1){
+    slide_c=0;
+}else{
+    slide_c++;
+}
+},3000)
 
 
 
